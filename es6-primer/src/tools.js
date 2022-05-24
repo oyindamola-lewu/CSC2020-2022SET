98ccb0030047f1708399c5e786ccffa2edf20970
Oyindamola J. Lewu <oyindamola.lewu@pau.edu.ng>
2022-05-24 10:30:38 +0100

    Updated Github to catch up to recent work

---
+++ es6-primer/src/tools.js
@@ -0,0 +1,7 @@
+const logger = output => {
+    console.log(output)
+}
+export const appName = "ES6 Review"; export const dummyFunction = () => {
+    let feedback = "I am a dummy function"; //mutable variable feedback = "I am still a dummy function";//value changed return feedback;
+}
+export default logger;
